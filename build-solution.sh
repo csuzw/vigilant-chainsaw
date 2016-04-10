46dc1893655db75df60747b471b65357628ec525
csuzw <rjcarrington@gmail.com>
2016-04-10 10:36:24 +0100

    FUCK FUCK FUCK FUCK FUCK

--- build-solution.sh
+++ build-solution.sh
@@ -2,6 +2,6 @@
 set -e # exit with nonzero exit code if anything fails
 
 # inject builder number
-sed -i "s@AssemblyVersion(\"1.0.*\")@AssemblyVersion("${APP_VERSION}.${TRAVIS_BUILD_NUMBER}")@g" ./src/SharedAssemblyInfo.cs
+#sed -i "s@AssemblyVersion(\"1.0.*\")@AssemblyVersion("${APP_VERSION}.${TRAVIS_BUILD_NUMBER}")@g" ./src/SharedAssemblyInfo.cs
 
 xbuild /p:Configuration=Release ./src/VigilantChainsaw.sln
