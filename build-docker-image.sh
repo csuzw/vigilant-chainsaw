4e225760fb1ebc0a3c5a4d63940186287a392758
csuzw <rjcarrington@gmail.com>
2016-04-10 13:52:47 +0100

    Docker build addition

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
+docker build -t csuzw/vigilant-chainsaw-application:${APP_VERSION}.${TRAVIS_BUILD_NUMBER} -t csuzw/vigilant-chainsaw-application:latest ./src/VigilantChainsaw.Application/bin/Release/
 docker login -e="${DOCKER_EMAIL}" -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker push csuzw/vigilant-chainsaw-application:latest
